ddb35ae8d781d0c5e039fb33e825f8d0e1752640
Pozsár Zsolt <pozsarzs@karak>
2019-11-22 20:19:46 +0100

    191122

--- source/frmmain.pas
+++ source/frmmain.pas
@@ -13,9 +13,6 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
-    Bevel1: TBevel;
-    Bevel2: TBevel;
-    Bevel3: TBevel;
     CheckBox1: TCheckBox;
     CheckBox10: TCheckBox;
     CheckBox11: TCheckBox;
@@ -33,109 +30,18 @@ type
     CheckBox22: TCheckBox;
     CheckBox23: TCheckBox;
     CheckBox24: TCheckBox;
-    CheckBox25: TCheckBox;
-    CheckBox26: TCheckBox;
-    CheckBox27: TCheckBox;
-    CheckBox28: TCheckBox;
-    CheckBox29: TCheckBox;
     CheckBox3: TCheckBox;
-    CheckBox30: TCheckBox;
-    CheckBox31: TCheckBox;
-    CheckBox32: TCheckBox;
-    CheckBox33: TCheckBox;
-    CheckBox34: TCheckBox;
-    CheckBox35: TCheckBox;
-    CheckBox36: TCheckBox;
-    CheckBox37: TCheckBox;
-    CheckBox38: TCheckBox;
-    CheckBox39: TCheckBox;
     CheckBox4: TCheckBox;
-    CheckBox40: TCheckBox;
-    CheckBox41: TCheckBox;
-    CheckBox42: TCheckBox;
-    CheckBox43: TCheckBox;
-    CheckBox44: TCheckBox;
-    CheckBox45: TCheckBox;
-    CheckBox46: TCheckBox;
-    CheckBox47: TCheckBox;
-    CheckBox48: TCheckBox;
-    CheckBox49: TCheckBox;
     CheckBox5: TCheckBox;
-    CheckBox50: TCheckBox;
-    CheckBox51: TCheckBox;
-    CheckBox52: TCheckBox;
-    CheckBox53: TCheckBox;
-    CheckBox54: TCheckBox;
-    CheckBox55: TCheckBox;
-    CheckBox56: TCheckBox;
-    CheckBox57: TCheckBox;
-    CheckBox58: TCheckBox;
-    CheckBox59: TCheckBox;
     CheckBox6: TCheckBox;
-    CheckBox60: TCheckBox;
-    CheckBox61: TCheckBox;
-    CheckBox62: TCheckBox;
-    CheckBox63: TCheckBox;
-    CheckBox64: TCheckBox;
-    CheckBox65: TCheckBox;
-    CheckBox66: TCheckBox;
-    CheckBox67: TCheckBox;
-    CheckBox68: TCheckBox;
-    CheckBox69: TCheckBox;
     CheckBox7: TCheckBox;
-    CheckBox70: TCheckBox;
-    CheckBox71: TCheckBox;
-    CheckBox72: TCheckBox;
-    CheckBox73: TCheckBox;
-    CheckBox74: TCheckBox;
-    CheckBox75: TCheckBox;
-    CheckBox76: TCheckBox;
-    CheckBox77: TCheckBox;
-    CheckBox78: TCheckBox;
-    CheckBox79: TCheckBox;
     CheckBox8: TCheckBox;
-    CheckBox80: TCheckBox;
-    CheckBox81: TCheckBox;
-    CheckBox82: TCheckBox;
-    CheckBox83: TCheckBox;
-    CheckBox84: TCheckBox;
-    CheckBox85: TCheckBox;
-    CheckBox86: TCheckBox;
-    CheckBox87: TCheckBox;
-    CheckBox88: TCheckBox;
-    CheckBox89: TCheckBox;
     CheckBox9: TCheckBox;
-    CheckBox90: TCheckBox;
-    CheckBox91: TCheckBox;
-    CheckBox92: TCheckBox;
-    CheckBox93: TCheckBox;
-    CheckBox94: TCheckBox;
-    CheckBox95: TCheckBox;
-    CheckBox96: TCheckBox;
     CheckGroup1: TCheckGroup;
-    CheckGroup2: TCheckGroup;
-    CheckGroup3: TCheckGroup;
-    CheckGroup4: TCheckGroup;
-    Label1: TLabel;
-    Label10: TLabel;
-    Label11: TLabel;
-    Label12: TLabel;
-    Label13: TLabel;
-    Label14: TLabel;
-    Label15: TLabel;
-    Label16: TLabel;
-    Label17: TLabel;
-    Label18: TLabel;
-    Label19: TLabel;
     Label2: TLabel;
-    Label20: TLabel;
     Label3: TLabel;
     Label4: TLabel;
     Label5: TLabel;
-    Label6: TLabel;
-    Label7: TLabel;
-    Label8: TLabel;
-    Label9: TLabel;
     MainMenu1: TMainMenu;
     MenuItem1: TMenuItem;
     MenuItem10: TMenuItem;
@@ -153,27 +59,13 @@ type
     MenuItem7: TMenuItem;
     MenuItem8: TMenuItem;
     MenuItem9: TMenuItem;
-    PageControl1: TPageControl;
     SpinEdit1: TSpinEdit;
-    SpinEdit10: TSpinEdit;
-    SpinEdit11: TSpinEdit;
-    SpinEdit12: TSpinEdit;
-    SpinEdit13: TSpinEdit;
-    SpinEdit14: TSpinEdit;
-    SpinEdit15: TSpinEdit;
     SpinEdit2: TSpinEdit;
     SpinEdit3: TSpinEdit;
     SpinEdit4: TSpinEdit;
-    SpinEdit5: TSpinEdit;
-    SpinEdit6: TSpinEdit;
-    SpinEdit7: TSpinEdit;
-    SpinEdit8: TSpinEdit;
-    SpinEdit9: TSpinEdit;
     StatusBar1: TStatusBar;
-    TabSheet1: TTabSheet;
-    procedure CheckBox2Change(Sender: TObject);
-    procedure CheckGroup4Click(Sender: TObject);
-    procedure ToolBar1Click(Sender: TObject);
+    ToolBar1: TToolBar;
+    TreeView1: TTreeView;
   private
     { private declarations }
   public
@@ -188,21 +80,6 @@ implementation
 {$R *.lfm}
 
 { TForm1 }
-
-procedure TForm1.CheckBox2Change(Sender: TObject);
-begin
-
-end;
-
-procedure TForm1.CheckGroup4Click(Sender: TObject);
-begin
-
-end;
-
-procedure TForm1.ToolBar1Click(Sender: TObject);
-begin
-
-end;
 
 end.
 
