f28570858bb04867349e8022c295cdb4ea282f0f
Pozsár Zsolt <pozsarzs@karak>
2019-11-27 21:36:54 +0100

    191127

--- source/frmmain.pas
+++ source/frmmain.pas
@@ -17,7 +17,7 @@ unit frmmain;
 interface
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Menus,
-  ComCtrls, ExtCtrls, StdCtrls, Spin, LResources, untcommonproc;
+  ComCtrls, ExtCtrls, StdCtrls, Spin, LResources, untcommonproc, dos;
 type
   { TForm1 }
   TForm1 = class(TForm)
@@ -44,6 +44,7 @@ type
     MenuItem7: TMenuItem;
     MenuItem8: TMenuItem;
     MenuItem9: TMenuItem;
+    SaveDialog1: TSaveDialog;
     SpinEdit1: TSpinEdit;
     SpinEdit2: TSpinEdit;
     SpinEdit3: TSpinEdit;
@@ -97,17 +98,24 @@ Resourcestring
   MESSAGE05='Lighting';
   MESSAGE06='Ventilating';
   MESSAGE07='Low external temp.';
-  MESSAGE08='';
-  MESSAGE09='';
-  MESSAGE10='';
-  MESSAGE17a='Maximal relative humidity [%]';
-  MESSAGE17b='Humidifier switch-off humidity [%]';
-  MESSAGE17c='Humidifier switch-on humidity [%]';
-  MESSAGE17d='Minimal relative humidity [%]';
-  MESSAGE28a='Maximal temperature [°C]';
-  MESSAGE28b='Heater switch-off temperature [°C]';
-  MESSAGE28c='Heater switch-on temperature [°C]';
-  MESSAGE28d='Minimal temperature [°C]';
+  MESSAGE08='INI files (*.ini)|*.ini|';
+  MESSAGE09='Text files (*.txt)|*.txt|';
+  MESSAGE10='Load from file';
+  MESSAGE11='Save to file';
+  MESSAGE12='Export to text file';
+  MESSAGE13='File exist, overwrite?';
+  MESSAGE14='ERROR: Cannot read file!';
+  MESSAGE15='ERROR: Cannot write file!';
+
+
+  MESSAGE17a='Minimal relative humidity [%]';
+  MESSAGE17b='Humidifier switch-on humidity [%]';
+  MESSAGE17c='Humidifier switch-off humidity [%]';
+  MESSAGE17d='Maximal relative humidity [%]';
+  MESSAGE28a='Minimal temperature [°C]';
+  MESSAGE28b='Heater switch-on temperature [°C]';
+  MESSAGE28c='Heater switch-off temperature [°C]';
+  MESSAGE28d='Maximal temperature [°C]';
   MESSAGE39a='Lights switch-on hour #1';
   MESSAGE39b='Lights switch-off hour #1';
   MESSAGE39c='Lights switch-on hour #2';
@@ -123,6 +131,145 @@ implementation
 
 {$R *.lfm}
 { TForm1 }
+
+function saveinifile(filename: string): boolean;
+var
+  iif: text;
+  b: byte;
+  s: string;
+const
+  HEADER1='; +----------------------------------------------------------------------------+';
+  HEADER2='; | XMMEEC v0.1 * Environment characteristics editor                           |';
+  HEADER3='; | Copyright (C) 2019 Pozsár Zsolt <pozsar.zsolt@.szerafingomba.hu>           |';
+  HEADER4='; | envir.ini                                                                  |';
+  HEADER5='; | growing environment characteristics                                        |';
+
+  function b2c(b: boolean): char;
+  begin
+    if b then b2c:='1' else b2c:='0';
+  end;
+
+begin
+  saveinifile:=true;
+  try
+    assign(iif,filename);
+    rewrite(iif);
+    writeln(iif,HEADER1);
+    writeln(iif,HEADER2);
+    writeln(iif,HEADER3);
+    writeln(iif,HEADER4);
+    writeln(iif,HEADER5);
+    writeln(iif,HEADER1);
+    writeln(iif,'');
+    writeln(iif,'[hyphae]');
+    writeln(iif,'; humidifier');
+    writeln(iif,'humidity_min=',inttostr(spineditvalues[1,1]));
+    writeln(iif,'humidifier_on=',inttostr(spineditvalues[1,2]));
+    writeln(iif,'humidifier_off=',inttostr(spineditvalues[1,3]));
+    writeln(iif,'humidity_max=',inttostr(spineditvalues[1,4]));
+    for b:=0 to 23 do
+    begin
+      s:='humidifier_disable_';
+      if b<10 then s:=s+'0';
+      s:=s+inttostr(b)+'='+b2c(checkgroupvalue[1,b]);
+      writeln(iif,s);
+    end;
+    writeln(iif,'');
+    writeln(iif,'; heaters');
+    writeln(iif,'temperature_min=',inttostr(spineditvalues[2,1]));
+    writeln(iif,'heater_on=',inttostr(spineditvalues[2,2]));
+    writeln(iif,'heater_off=',inttostr(spineditvalues[2,3]));
+    writeln(iif,'temperature_max=',inttostr(spineditvalues[2,4]));
+    for b:=0 to 23 do
+    begin
+      s:='heater_disable_';
+      if b<10 then s:=s+'0';
+      s:=s+inttostr(b)+'='+b2c(checkgroupvalue[2,b]);
+      writeln(iif,s);
+    end;
+    writeln(iif,'');
+    writeln(iif,'; lights');
+    writeln(iif,'light_on1=',inttostr(spineditvalues[3,1]));
+    writeln(iif,'light_off1=',inttostr(spineditvalues[3,2]));
+    writeln(iif,'light_on2=',inttostr(spineditvalues[3,3]));
+    writeln(iif,'light_off2=',inttostr(spineditvalues[3,4]));
+    writeln(iif,'');
+    writeln(iif,'; ventillators');
+    writeln(iif,'vent_on=',inttostr(spineditvalues[4,1]));
+    writeln(iif,'vent_off=',inttostr(spineditvalues[4,2]));
+    for b:=0 to 23 do
+    begin
+      s:='vent_disable_';
+      if b<10 then s:=s+'0';
+      s:=s+inttostr(b)+'='+b2c(checkgroupvalue[4,b]);
+      writeln(iif,s);
+    end;
+    writeln(iif,'vent_lowtemp=',inttostr(spineditvalues[5,1]));
+    for b:=0 to 23 do
+    begin
+      s:='vent_disablelowtemp_';
+      if b<10 then s:=s+'0';
+      s:=s+inttostr(b)+'='+b2c(checkgroupvalue[5,b]);
+      writeln(iif,s);
+    end;
+    writeln(iif,'');
+    writeln(iif,'[mushroom]');
+    writeln(iif,'; humidifier');
+    writeln(iif,'humidity_min=',inttostr(spineditvalues[7,1]));
+    writeln(iif,'humidifier_on=',inttostr(spineditvalues[7,2]));
+    writeln(iif,'humidifier_off=',inttostr(spineditvalues[7,3]));
+    writeln(iif,'humidity_max=',inttostr(spineditvalues[7,4]));
+    for b:=0 to 23 do
+    begin
+      s:='humidifier_disable_';
+      if b<10 then s:=s+'0';
+      s:=s+inttostr(b)+'='+b2c(checkgroupvalue[7,b]);
+      writeln(iif,s);
+    end;
+    writeln(iif,'');
+    writeln(iif,'; heaters');
+    writeln(iif,'temperature_min=',inttostr(spineditvalues[8,1]));
+    writeln(iif,'heater_on=',inttostr(spineditvalues[8,2]));
+    writeln(iif,'heater_off=',inttostr(spineditvalues[8,3]));
+    writeln(iif,'temperature_max=',inttostr(spineditvalues[8,4]));
+    for b:=0 to 23 do
+    begin
+      s:='heaters_disable_';
+      if b<10 then s:=s+'0';
+      s:=s+inttostr(b)+'='+b2c(checkgroupvalue[8,b]);
+      writeln(iif,s);
+    end;
+    writeln(iif,'');
+    writeln(iif,'; lights');
+    writeln(iif,'light_on1=',inttostr(spineditvalues[9,1]));
+    writeln(iif,'light_off1=',inttostr(spineditvalues[9,2]));
+    writeln(iif,'light_on2=',inttostr(spineditvalues[9,3]));
+    writeln(iif,'light_off2=',inttostr(spineditvalues[9,4]));
+    writeln(iif,'');
+    writeln(iif,'; ventillators');
+    writeln(iif,'vent_on=',inttostr(spineditvalues[10,1]));
+    writeln(iif,'vent_off=',inttostr(spineditvalues[10,2]));
+    for b:=0 to 23 do
+    begin
+      s:='vent_disable_';
+      if b<10 then s:=s+'0';
+      s:=s+inttostr(b)+'='+b2c(checkgroupvalue[10,b]);
+      writeln(iif,s);
+    end;
+    writeln(iif,'vent_lowtemp=',inttostr(spineditvalues[11,1]));
+    for b:=0 to 23 do
+    begin
+      s:='vent_disablelowtemp_';
+      if b<10 then s:=s+'0';
+      s:=s+inttostr(b)+'='+b2c(checkgroupvalue[11,b]);
+      writeln(iif,s);
+    end;
+    writeln(iif,'');
+    close(iif);
+  except
+    saveinifile:=false;
+  end;
+end;
 
 // clear all values;
 procedure TForm1.MenuItem2Click(Sender: TObject);
@@ -148,8 +295,29 @@ end;
 
 // save ini file
 procedure TForm1.MenuItem5Click(Sender: TObject);
-begin
+var
+   f: string;
+   tfdir, tfname, tfext: shortstring;
 
+begin
+  with SaveDialog1 do
+  begin
+    Filter:=MESSAGE08;
+    InitialDir:=untcommonproc.userdir;
+    Title:=MESSAGE11;
+    FilterIndex := 1;
+    if not Execute then exit;
+    f:=FileName;
+  end;
+  if length(f)=0 then exit;
+  fsplit(f,tfdir,tfname,tfext);
+  if FSearch(tfname+tfext,tfdir)<>'' then
+    if MessageDlg(MESSAGE13,mtConfirmation,[mbYes,mbNo],0)=mrNo then exit;
+  try
+    saveinifile(f);
+  except
+    ShowMessage(MESSAGE15);
+  end;
 end;
 
 // export to text file
@@ -207,6 +375,7 @@ var
   b: byte;
 begin
   // set widgets
+  Button1.Enabled:=visiblespinedits[TreeView1.Selected.AbsoluteIndex,1];
   Label1.Visible:=visiblespinedits[TreeView1.Selected.AbsoluteIndex,1];
   Label2.Visible:=visiblespinedits[TreeView1.Selected.AbsoluteIndex,2];
   Label3.Visible:=visiblespinedits[TreeView1.Selected.AbsoluteIndex,3];
