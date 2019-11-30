6cb08872c89be269b2522769cc157f5b707a8472
Pozsár Zsolt <pozsarzs@gmail.com>
2019-11-30 21:30:48 +0100

    191130

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ rem  build.bat
 rem  Utility for build/install/uninstall application on Windows
 rem ----------------------------------------------------------------------------
 
-set PPC=c:\lazarus\fpc\2.6.4\bin\i386-win32\ppc386.exe
+set PPC=c:\lazarus\fpc\3.0.4\bin\i386-win32\ppc386.exe
 set LAZ=c:\lazarus\
 set NAME=xmmeec
 set /p VERSION=<documents\VERSION
@@ -45,7 +45,7 @@ set FPFLAG4=-FE.\lib\%ARCH%-%OS% -dLCL -dLCLwin32 -Fu%LAZ%\packager\units\%ARCH%
 
 cd source
 echo Compiling source code...
-copy config.inc.in config.inc
+copy config.pas.in config.pas
 echo.
 %PPC% %FPFLAG1% %FPFLAG2% %FPFLAG3% %FPFLAG4% xmmeec.lpr
 echo.
@@ -73,19 +73,21 @@ echo Selected target folder: %INSTDIR%
 echo %INSTDIR% > install.log
 if not exist "source\lib\%ARCH%-%OS%\xmmeec.exe" ( echo Error: firstly run "build.bat" to compile source code! & goto end )
 echo Installing application...
-md %INSTDIR%
+md "%INSTDIR%"
 if not errorlevel 0 ( echo Error: cannot install application! & goto end )
-md %INSTDIR%\documents
-copy /y documents\*.* %INSTDIR%\documents\
-del /q %INSTDIR%\documents\Makefile
-md %INSTDIR%\languages
-copy /y languages\*.pot %INSTDIR%\languages\
-md %INSTDIR%\languages\hu
-copy /y languages\%NAME%_hu.mo %INSTDIR%\languages\hu\%NAME%.mo
-del /q %INSTDIR%\languages\Makefile
-copy /y README*.* %INSTDIR%\
-copy /y source\lib\%ARCH%-%OS%\*.exe %INSTDIR%\
-config\mkshortcut.vbs /target:%INSTDIR%\%NAME%.exe /shortcut:%USERPROFILE%\desktop\XMMEEC"
+md "%INSTDIR%\documents"
+copy /y documents\*.* "%INSTDIR%\documents\"
+del /q "%INSTDIR%\documents\Makefile"
+md "%INSTDIR%\languages"
+copy /y languages\*.pot "%INSTDIR%\languages\"
+md "%INSTDIR%\languages\hu"
+copy /y languages\%NAME%_hu.mo "%INSTDIR%\languages\hu\%NAME%.mo"
+md "%INSTDIR%\settings"
+copy /y settings\*.* "%INSTDIR%\settings\"
+del /q "%INSTDIR%\settings\Makefile"
+copy /y README*.* "%INSTDIR%\"
+copy /y source\lib\%ARCH%-%OS%\*.exe "%INSTDIR%\"
+config\mkshortcut.vbs /target:"%INSTDIR%\%NAME%.exe" /shortcut:"%USERPROFILE%\desktop\XMMEEC"
 echo.
 echo Run 'build.bat uninstall' if you remove this application.
 goto end
@@ -93,12 +95,12 @@ goto end
 rem - uninstall application
 :uninstall
 if not exist "install.log" ( echo Error: install.log file not found, cannot uninstall application! & goto end )
-set /p %INSTDIR%=<install.log
+set /p "%INSTDIR%"=<install.log
 echo Check application's folder: %INSTDIR%
 if not exist "%INSTDIR%" ( echo Error: bad folder name in install.log file, cannot uninstall application! & goto end )
 echo Removing application...
-rd /s %INSTDIR%
-del /q %USERPROFILE%\desktop\XMMEEC.lnk
+rd /s "%INSTDIR%"
+del /q "%USERPROFILE%\desktop\XMMEEC.lnk"
 goto end
 
 :end
